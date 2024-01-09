a2c0056e92ab9494913e6a1bb2e4eecbfb177816
utsav <utsavadhikari452@gmail.com>
2024-01-09 14:12:11 +0545

    Created a layout page for the admin Area.

--- Guild/Areas/Admin/Controllers/AdminController.cs
+++ Guild/Areas/Admin/Controllers/AdminController.cs
@@ -2,12 +2,15 @@
 
 namespace Guild.Areas.Admin.Controllers
 {
+
     [Area("Admin")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+       public IActionResult Index()
         {
             return View("~/Areas/Admin/Views/Admin/AdminDash.cshtml");
         }
     }
 }
+
+
